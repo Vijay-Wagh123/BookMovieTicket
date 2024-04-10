38dba49e8051ceb8701a1055d730012f1f4515b7
Vijay Gorakh Wagh <166473330+Vijay-Wagh123@users.noreply.github.com>
2024-04-10 19:19:43 +0530

    Update BookTicket.java

--- BookTicket.java
+++ BookTicket.java
@@ -1,10 +1,36 @@
 package Java_project;
+class Manage{
+	int total_s=10;
+	 synchronized public void book(int seats) {
+		if(total_s>=seats) {
+			System.out.println("Your seats are booked succefully:");
+			total_s=total_s-seats;
+			System.out.println("Remaing seats:"+total_s);
+		}
+		else {
+			System.out.println("Your seats are not booked:");
+			System.out.println("Remaing seats:"+total_s);
+		}
+	}
+}
 
-public class BookTicket {
 
+public class BookTicket extends Thread {
+	static Manage v;
+	int seats;
+	public void run() {
+		v.book(seats);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		v=new Manage();
+		BookTicket s=new BookTicket();
+		s.seats=7;
+		s.start();
+		
+		BookTicket s1=new BookTicket();
+		s1.seats=6;
+		s1.start();
 	}
 
 }
+
